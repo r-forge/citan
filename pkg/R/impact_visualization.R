7eef284a6ceec1a5214b4866bd7d6bd4ec5027b4
gagolews (Dr. ) <egzaltowany@gmail.com>
2012-06-08 08:28:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/citan@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/impact_visualization.R
+++ pkg/R/impact_visualization.R
@@ -18,53 +18,53 @@
 
 
 #' The citation function of a vector \eqn{x=(x_1,\dots,x_n)}
-#' is a mapping \deqn{\pi(y)=x_{(n-\lfloor y+1\rfloor)}}{\pi(y)=x_{(n-floor(y+1)+1)}}
+#' is a mapping \deqn{\pi(y)=x_{(n-\lfloor y+1\rfloor+1)}}{\pi(y)=x_{(n-floor(y+1)+1)}}
 #' defined for \eqn{0\le y<n}, where \eqn{x_{(i)}} denotes the
 #' \eqn{i}-th smallest value of \eqn{x}.
 #'
 #' @title Draw the citation function of a given vector
 #' @param x non-negative numeric vector.
 #' @param xmarg x-margin on the right.
-#' @param add logical; indicates whether to start a new plot, \code{TRUE} by default.
+#' @param add logical; indicates whether to start a new plot, \code{FALSE} by default.
 #' @param ylim,xlim,xlab,ylab,main,... additional graphical parameters.
 #' @seealso \code{\link{curve.add.rp}}, \code{\link{curve.add.lp}}, \code{\link{plot.default}}
 #' @examples
 #' john_s <- c(11,5,4,4,3,2,2,2,2,2,1,1,1,0,0,0,0);
 #' plot.citfun(john_s, main="Smith, John", col="red");
 #' @export
-plot.citfun <- function(x, ..., xmarg=10, add=F, xlab="", ylab="", main="", ylim=c(0, max(x)), xlim=c(0,length(x)+xmarg))
+plot.citfun <- function(x, ..., xmarg=10, add=FALSE, xlab="", ylab="", main="", ylim=c(0, max(x)), xlim=c(0,length(x)+xmarg))
 {
-	n <- length(x);
-	if (n == 0) stop("Zero-length vector given.");
-	if (mode(x) != "numeric") stop("Non-numeric vector given.");
-	if (any(x < 0)) warning("The function should be used with non-negative vectors.");
+   n <- length(x);
+   if (n == 0) stop("Zero-length vector given.");
+   if (mode(x) != "numeric") stop("Non-numeric vector given.");
+   if (any(x < 0)) warning("The function should be used with non-negative vectors.");
 
-	x <- sort(x, decreasing=T);
+   x <- sort(x, decreasing=T);
 
-	px <- n;
-	py <- x[n];
+   px <- n;
+   py <- x[n];
 
-	for (i in (n-1):1)
-	{
-		if (x[i]!=x[i+1])
-		{
-			px <- c(i,    px);
-			py <- c(x[i], py);
-		}
-	}
+   for (i in (n-1):1)
+   {
+      if (x[i]!=x[i+1])
+      {
+         px <- c(i,    px);
+         py <- c(x[i], py);
+      }
+   }
 
 
 
-	if (!add)
-	{
-		plot(px, py, ylim=ylim, xlim=xlim, type='p', xlab=xlab, ylab=ylab, main=main, ...);
-	} else
-	{
-		points(px, py, ...);
-	}
+   if (!add)
+   {
+      plot(px, py, ylim=ylim, xlim=xlim, type='p', xlab=xlab, ylab=ylab, main=main, ...);
+   } else
+   {
+      points(px, py, ...);
+   }
 
 # 	segments(px, py, px, c(py[-1],0), ...);
-	segments(c(0,px[-length(px)]), py, px, py, ...);
+   segments(c(0,px[-length(px)]), py, px, py, ...);
 }
 
 
@@ -90,22 +90,22 @@ plot.citfun <- function(x, ..., xmarg=10, add=F, xlab="", ylab="", main="", ylim
 #' @export
 curve.add.rp <- function(r, p=Inf, n=101, ...)
 {
-	if (length(p) != 1 || mode(p) != "numeric") stop("p must be a single numeric value");
-	if (p < 1) stop("p must be >= 1");
+   if (length(p) != 1 || mode(p) != "numeric") stop("p must be a single numeric value");
+   if (p < 1) stop("p must be >= 1");
 
-	if (length(r) != 1 || mode(r) != "numeric") stop("r must be a single numeric value");
-	if (r <= 0) stop("r must be > 0");
+   if (length(r) != 1 || mode(r) != "numeric") stop("r must be a single numeric value");
+   if (r <= 0) stop("r must be > 0");
 
-	if (is.finite(p))
-	{
-		px <- seq(0,r,length=n);
-		py <- (r^p-px^p)^(1.0/p);
-		lines(px, py, ...);
-	} else
-	{
-		lines(c(0,r), c(r,r), ...);
-		points(r, r, ...);
-	}
+   if (is.finite(p))
+   {
+      px <- seq(0,r,length=n);
+      py <- (r^p-px^p)^(1.0/p);
+      lines(px, py, ...);
+   } else
+   {
+      lines(c(0,r), c(r,r), ...);
+      points(r, r, ...);
+   }
 }
 
 
@@ -131,23 +131,23 @@ curve.add.rp <- function(r, p=Inf, n=101, ...)
 #' @export
 curve.add.lp <- function(ab, p=Inf, n=101, ...)
 {
-	if (length(p) != 1 || mode(p) != "numeric") stop("p must be a single numeric value");
-	if (p < 1) stop("p must be >= 1");
+   if (length(p) != 1 || mode(p) != "numeric") stop("p must be a single numeric value");
+   if (p < 1) stop("p must be >= 1");
 
-	if (length(ab) != 2 || mode(ab) != "numeric") stop("ab must be a numeric vector of length 2");
-	if (any(ab <= 0)) stop("ab must be > 0");
+   if (length(ab) != 2 || mode(ab) != "numeric") stop("ab must be a numeric vector of length 2");
+   if (any(ab <= 0)) stop("ab must be > 0");
 
-	a <- ab[1];
-	b <- ab[2];
+   a <- ab[1];
+   b <- ab[2];
 
-	if (is.finite(p))
-	{
-		px <- seq(0,a,length=n);
-		py <- (b^p-(b/a*px)^p)^(1.0/p);
-		lines(px, py, ...);
-	} else
-	{
-		lines(c(0,a), c(b,b), ...);
-		points(a, b, ...);
-	}
+   if (is.finite(p))
+   {
+      px <- seq(0,a,length=n);
+      py <- (b^p-(b/a*px)^p)^(1.0/p);
+      lines(px, py, ...);
+   } else
+   {
+      lines(c(0,a), c(b,b), ...);
+      points(a, b, ...);
+   }
 }
